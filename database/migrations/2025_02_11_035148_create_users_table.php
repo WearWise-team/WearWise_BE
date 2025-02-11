339404ddab95372dba15494b61341159df77be36
Hung2201 <duchung12a8@gmail.com>
2025-02-11 11:07:45 +0700

    WW-44/completed creating table users, suppliers and products

---
+++ database/migrations/2025_02_11_035148_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('password');
+            $table->string('email')->unique();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('avatar')->default('https://s.net.vn/76Th');
+            $table->string('weight')->nullable();
+            $table->string('height')->nullable();
+            $table->string('bust')->nullable();
+            $table->string('waist')->nullable();
+            $table->string('hips')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
