7526238c23d1863d1ac09467f136c1b2569b318c
Hung2201 <duchung12a8@gmail.com>
2025-02-17 09:03:10 +0700

    WW-44 Register binding in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,20 @@
 
 namespace App\Providers;
 
+use App\Repositories\Implementations\OrderRepository;
+use App\Repositories\Implementations\WishlistRepository;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Contracts\IRepository;
+use App\Repositories\Implementations\Cart_ItemRepository;
+use App\Repositories\Implementations\CartRepository;
+use App\Repositories\Implementations\Discount_AssignmentRepository;
+use App\Repositories\Implementations\DiscountRepository;
+use App\Repositories\Implementations\Order_ItemRepository;
 use App\Repositories\Implementations\UserRepository;
 use App\Repositories\Implementations\ProductRepository;
+use App\Repositories\Implementations\ReviewRepository;
+use App\Repositories\Implementations\SizeRepository;
+use App\Repositories\Implementations\SupplierRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +25,17 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(IRepository::class, UserRepository::class);
+        $this->app->bind(IRepository::class, SupplierRepository::class);
         $this->app->bind(IRepository::class, ProductRepository::class);
+        $this->app->bind(IRepository::class, ReviewRepository::class);
+        $this->app->bind(IRepository::class, WishlistRepository::class);
+        $this->app->bind(IRepository::class, OrderRepository::class);
+        $this->app->bind(IRepository::class, CartRepository::class);
+        $this->app->bind(IRepository::class, DiscountRepository::class);
+        $this->app->bind(IRepository::class, Order_ItemRepository::class);
+        $this->app->bind(IRepository::class, Cart_ItemRepository::class);
+        $this->app->bind(IRepository::class, Discount_AssignmentRepository::class);
+        $this->app->bind(IRepository::class, SizeRepository::class);
     }
 
     /**
