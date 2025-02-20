4d5cb9a66853fb6e5c5612fb8ea6edbae25f1ab6
Hung2201 <duchung12a8@gmail.com>
2025-02-20 21:36:20 +0700

    WW-138 Completed writing api for search product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -49,9 +49,10 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        $product = $this->productService->getProductById($id);
+        $product = $this->productService->getProductById((int) $id);
         return response()->json($product);
     }
+
 
     /**
      * Update the specified product in the database.
@@ -81,10 +82,14 @@ class ProductController extends Controller
         return response()->json(null, 204);
     }
 
-    public function search(Request $request)
+    public function searchProductByName(Request $request)
     {
-        $name = $request -> input('name');
-        $products = $this->productService->searchProductByName($name);
-        return response() -> json($products);
+        $request->validate([
+            'name' => 'required|string|min:2'
+        ]);
+
+        $products = $this->productService->searchProductByName($request->input('name'));
+
+        return response()->json($products);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,8 @@ Route::post('products', [ProductController::class, 'store']);
 Route::get('products/{id}', [ProductController::class, 'show']);
 Route::put('products/{id}', [ProductController::class, 'update']);
 Route::delete('products/{id}', [ProductController::class, 'destroy']);
-Route::get('/products/search', [ProductController::class, 'search']);
+Route::post('/products/search', [ProductController::class, 'searchProductByName']);
+
 
 Route::apiResource('users', controller: UserController::class);
 
